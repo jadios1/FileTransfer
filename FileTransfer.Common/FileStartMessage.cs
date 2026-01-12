392e2e49b4abbd137d6ae92bc06741e564124319
jadios <evsky.yan@gmail.com>
2026-01-12 03:09:53 +0100

    Zaczynam robic server

---
+++ FileTransfer.Common/FileStartMessage.cs
@@ -0,0 +1,3 @@
+namespace FileTransfer.Common;
+
+public class FileStartMessage;
---
+++ FileTransfer.Server/FileTransferServer.cs
@@ -0,0 +1,52 @@
+namespace FileTransfer.Server;
+using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+public class FileTransferServer
+{
+    private Int32 _port;
+    private IPAddress _ipAddress;
+    private TcpListener _server;
+
+    public FileTransferServer(Int32 port, string ipAddress)
+    {
+        _port = port;
+        _ipAddress = IPAddress.Parse(ipAddress);
+        _server = new TcpListener(_ipAddress,_port);
+    }
+
+    public void StartListening()
+    {
+        _server.Start();
+    }
+
+    public void HandleClients()
+    {
+        Byte[] bytes = new Byte[256];
+        String data = null;
+
+        while (true)
+        {
+            Console.WriteLine("Waiting for connection...");
+            using TcpClient client = _server.AcceptTcpClient();
+            Console.WriteLine("Connected!");
+
+            data = null;
+
+            NetworkStream stream = client.GetStream();
+
+            int i = 0;
+
+            while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
+            {
+                continue;
+            }
+
+        }
+    }
+
+
+}
